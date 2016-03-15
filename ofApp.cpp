7308e736b6b8e2cbf290cabfd23b447b6436c0dd
rpavl <robertpavlovskis@hotmail.com>
2016-03-15 20:35:40 +0000

    Parallax scroll added

--- ofApp.cpp
+++ ofApp.cpp
@@ -1,275 +1,126 @@
-
-
-
 #include "ofApp.h"
-
-
-//-------------------------------------------------------------
-ofApp::~ofApp() {
-
-
-}
 
 //--------------------------------------------------------------
 void ofApp::setup() {
+	img[0].loadImage("japan3.bmp");
+	img[1].loadImage("mandle3.bmp");
 
-	//prepare quadric for sphere
-	quadric = gluNewQuadric();
-	gluQuadricTexture(quadric, GL_TRUE);
-	gluQuadricNormals(quadric, GLU_SMOOTH);
+	width = img[0].getWidth();
+	height = img[0].getHeight();
 
+	pixelout = new unsigned char[width* height * 3];//Assigns length to array
+	pixelin = new unsigned char[width* height * 3];
 
-	//Recorder initlisers
-	recorder.setPrefix(ofToDataPath("recording1/frame")); // this directory must already exist
-	recorder.setFormat("bmp"); // png is really slow but high res, bmp is fast but big, jpg is just right
 }
-
 //--------------------------------------------------------------
-void ofApp::update() {
+void ofApp::update(){
 
-	for (i = 0; i < videos.size(); i++)
+	for (u = 0; u < 2; u++) {
 
-			if (!skip[i])//Skips video if update disabled
-				videos[i].update();
+		pixelin = img[u].getPixels();//Gets pixels from video
 
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
 
-	if (videos.size() > 0) {//Prevents errors if video hasn't been uploaded
 
-		width = videos[0].getWidth();
-		height = videos[0].getHeight();
+				thre = 254;
 
-		if (!allocate) {//Allocates memeory to video once it has been uploaded
-			pixelout = new unsigned char[width*height * 3];//Assigns length to array
-			pixelin = new unsigned char[width*height * 3];
-			allocate = true;
-		}
+				pointer[0] = (j*width + i);
+				pointer[u+1] = (loop(j + mouseY * (int)(5 * (u+1)), 0, height - 1)*width + loop(i + mouseX * (int)(5 * (u + 1)), 0, width - 1));
+				
 
-		if (cycleOn) {
-			cycle();//cycle throguh all the frames starting at 0 for recording
-		}
-		
-		if (newFrame) { //Check if new there is new frame
-			addNewFrame(); //Add new frame once passed through the filters
-		}
-	}
-}
+				if (u == 0 || pixelin[pointer[2] * 3 + 1] < thre) {
 
-//--------------------------------------------------------------
-void ofApp::draw() {
+					
 
-	ofBackground(125);
-	ofSetColor(255);
-
-	if(toggle)//Activated for 360 preview
-	cam.begin();
-
-	for (i = 0; i < videos.size(); i++)
-		videos[i].draw((ofGetWidth() / videos.size()*i), 0, ofGetWidth() / videos.size(), ofGetHeight() / videos.size());
-
-
-	if (videos.size() > 0) { //If there are videos preform recording functions
-
-
-		if (preLoad) { //Stops recording if ran out of frames
-
-			recorder.stopThread();
-			rec = false;
-
-			//Get directory size
-			dir.listDir("recording1");
-
-			sequence.loadSequence("recording1/frame", "bmp", 0, dir.size() - 1, 4);
-			sequence.preloadAllFrames();	//this way there is no stutter when loading frames
-			sequence.setFrameRate(5); //set to ten frames per second
-			sequenced = true; //Plays sequence when it has been written
-
-			for (int j = 0; j < videos.size(); j++)
-				skip[j] = true; //Prevents original videos from playing because composite has loaded, saving memory
-
-			preLoad = false;
-		}
-	}
-
-	if (sequenced) //Checks if sequence has been created, plays it
-		sequence.getFrameAtPercent(phasor(0.005, 0, 1))->draw(ofGetWidth()*0.5, ofGetHeight()*0.5, ofGetWidth()*0.5, ofGetHeight()*0.5);
-
-
-	if (toggle) { //Activated for 360 preview
-		wrapSphere();
-		cam.end();
-	}
-
-}
-
-void ofApp::exit() {
-	recorder.waitForThread();
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-
-	if (key == 'r') { //Recording
-		cycleOn = true;//cycleOns through frames individually when recording
-
-		recorder.setCounter(0);
-		for (i = 0; i < videos.size(); i++)
-			videos[i].setFrame(0);
-		rec = true;
-		recorder.startThread(false, true);
-
-
-		for (int j = 0; j < videos.size(); j++)
-			skip[j] = false;
-
-		for (i = 0; i < videos.size(); i++)
-			videos[i].setPaused(true);
-	}
-
-	if (key == 't') { //Turns on sphere and camera controls
-		cam.toggleControl();
-		toggle = !toggle;
-	}
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo info) {
-
-	//Gets video path
-	path = info.files[0];
-
-	//Creates new video player
-	ofVideoPlayer player;
-	player.loadMovie(path);
-	player.setLoopState(OF_LOOP_NORMAL);
-	player.play();
-	videos.push_back(player);
-
-}
-
-//--------------------------------------------------------------
-
-double ofApp::phasor(double frequency, double startphase, double endphase) {
-	//This is a phasor that takes a value for the start and end of the ramp. 
-	if (phase<startphase) {
-		phase = startphase;
-	}
-	if (phase >= endphase) phase = startphase;
-	phase += ((endphase - startphase) / (1 / (frequency)));
-	return(phase);
-}
-
-
-//--------------------------------------------------------------
-void ofApp::wrapSphere() {
-
-	ofSetColor(255, 255, 255);
-
-	sequence.getTextureReference().bind();
-
-	ofPushMatrix();
-
-	ofRotateX(90);
-
-	glMatrixMode(GL_TEXTURE);
-	glPushMatrix();
-	ofScale(sequence.getWidth(), sequence.getHeight());
-	glMatrixMode(GL_MODELVIEW);
-
-	glEnable(GL_DEPTH_TEST); //enable depth comparisons NEEDED FOR SPHERE DEPTH
-	ofDisableArbTex(); //needed for textures to work with gluSphe
-
-	gluSphere(quadric, 200, 100, 100);
-
-	glDisable(GL_DEPTH_TEST);//disable depth comparisons NEEDED FOR FLAT MASK
-	ofEnableArbTex(); //needed for textures to work with gluSphe
-
-
-	glMatrixMode(GL_TEXTURE);
-	glPopMatrix();
-	glMatrixMode(GL_MODELVIEW);
-
-	sequence.getTextureReference().unbind();
-
-	ofPopMatrix();
-}
-
-
-//--------------------------------------------------------------
-void ofApp::addNewFrame() {
-
-	for (u = 0; u < videos.size(); u++) {
-		if (rec) {
-			pixelin = videos[u].getPixels();//Gets pixels from video
-
-			for (i = 0; i < width; i++) {
-				for (j = 0; j < height; j++) { //Writes pixels if chroma trheshold hasn't been reached, otherwise assumes from previous frame
-
-
-					thre = 254;
-
-					if (u == 0 || pixelin[(j*width + i) * 3 + 1] < thre) {
-						pixelout[(j*width + i) * 3 + 0] = pixelin[(j*width + i) * 3 + 0];//red
-						pixelout[(j*width + i) * 3 + 1] = pixelin[(j*width + i) * 3 + 1];//green
-						pixelout[(j*width + i) * 3 + 2] = pixelin[(j*width + i) * 3 + 2];//blue
-					}
+					pixelout[pointer[0] * 3 + 0] = pixelin[pointer[u + 1] * 3 + 0];//red
+					pixelout[pointer[0] * 3 + 1] = pixelin[pointer[u + 1] * 3 + 1];//green
+					pixelout[pointer[0] * 3 + 2] = pixelin[pointer[u + 1] * 3 + 2];//blue
 				}
-			}
-			preview.setFromPixels(pixelout, width, height, OF_IMAGE_COLOR);//Writes pixels to preview sequence
-		}
 
+			}
+		}
 	}
-	//Outside of loop to make sure final edit is layered correctly
-	recorder.addFrame(preview);
-	newFrame = false;
+
+
+	preview.setFromPixels(pixelout, width, height, OF_IMAGE_COLOR);//Writes pixels to preview sequence
 }
 
 //--------------------------------------------------------------
-void ofApp::cycle() { //cycle throguh all the frames starting at 0 for recording
-	for (i = 0; i < videos.size(); i++) {
+void ofApp::draw(){
+	counter+=40;
+	preview.draw(0, 0, width*1.55, height*1.55);
 
-		videos[i].nextFrame();
-		newFrame = true;
 
-		if (videos[0].getCurrentFrame() >= videos[0].getTotalNumFrames() || videos[0].getPosition() >= 0.99) {
-			videos[i].setFrame(0);
-			cycleOn = false;
-			preLoad = true;
-		}
-	}
+	ofDrawBitmapString(loop(mouseX-(ofGetWidth()/2), 0, 50), ofGetWidth()*0.85, ofGetHeight()*0.5);
 }
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+
+int ofApp::loop(int current, int start, int end) {
+
+	if (current > end)
+		phase = current - (end * (int)(current / end));
+	else if(current < start)
+		phase = current + (end * (((int)(current / end)*-1)+1));
+	else
+		phase = current;
+
+	return phase;
+}
+
