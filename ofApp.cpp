9572f3110b52109e9b52b17ddcabadd4b0ad04f7
rpavl <robertpavlovskis@hotmail.com>
2016-03-21 17:31:45 +0000

    Image convolution kernel, better green screen

--- ofApp.cpp
+++ ofApp.cpp
@@ -1,297 +1,161 @@
-
-
-
 #include "ofApp.h"
 
+//--------------------------------------------------------------
+void ofApp::setup(){
 
-//-------------------------------------------------------------
-ofApp::~ofApp() {
+	img[0].loadImage("japan.bmp");
+	img[1].loadImage("hades.bmp");
 
+	width = img[0].getWidth();
+	height = img[0].getHeight();
+
+	pixelout = new unsigned char[width* height * 3];//Assigns length to array
+	pixelin = new unsigned char[width* height * 3];
+	pixels = new unsigned char[width* height * 3];
+
+
+	ofSetVerticalSync(true);
+
+	gui.setup(); // most of the time you don't need a name
+	gui.add(thre.setup("Threshold", 254, 0, 256));
+
+
+	matrixsize = sqrt(sizeof(matrix) / sizeof(*matrix));
+	range = floor(matrixsize / 2);
 
 }
 
 //--------------------------------------------------------------
-void ofApp::setup() {
+void ofApp::update(){
 
-	//prepare quadric for sphere
-	quadric = gluNewQuadric();
-	gluQuadricTexture(quadric, GL_TRUE);
-	gluQuadricNormals(quadric, GLU_SMOOTH);
 
+	for (u = 0; u < 2; u++) {
 
-	//Recorder initlisers
-	recorder.setPrefix(ofToDataPath("recording1/frame")); // this directory must already exist
-	recorder.setFormat("bmp"); // png is really slow but high res, bmp is fast but big, jpg is just right
+		pixelin = img[u].getPixels();//Gets pixels from video
 
+		for (int i = range; i < width-range; i++) {
+			for (int j = range; j < height-range; j++) {
 
 
+				pointer[0] = (j*width + i);
+				red = pixelin[pointer[0] * 3 + 0];//red
+				green = pixelin[pointer[0] * 3 + 1];//green
+				blue = pixelin[pointer[0] * 3 + 2];//blue
 
 
+				if (u == 0 ||  green <= blue || green <= red || green < thre) {
 
-	recorder.stopThread();
-	rec = false;
 
-	//Get directory size
-	dir.listDir("recording1");
+					pixelout[pointer[0] * 3 + 0] = red;
+					pixelout[pointer[0] * 3 + 1] = green;
+					pixelout[pointer[0] * 3 + 2] = blue;
 
-	sequence.loadSequence("recording1/frame", "bmp", 0, dir.size() - 1, 4);
-	sequence.preloadAllFrames();	//this way there is no stutter when loading frames
-	sequence.setFrameRate(5); //set to ten frames per second
-	sequenced = true; //Plays sequence when it has been written
+					//pixelout[pointer[0] * 3 + 2] = convolution(i, j, matrix, matrixsize, 2, pixelin);//blue
 
-	for (int j = 0; j < videos.size(); j++)
-		skip[j] = true; //Prevents original videos from playing because composite has loaded, saving memory
-
-	preLoad = false;
-}
-
-//--------------------------------------------------------------
-void ofApp::update() {
-
-	for (i = 0; i < videos.size(); i++)
-
-			if (!skip[i])//Skips video if update disabled
-				videos[i].update();
-
-
-	if (videos.size() > 0) {//Prevents errors if video hasn't been uploaded
-
-		width = videos[0].getWidth();
-		height = videos[0].getHeight();
-
-		if (!allocate) {//Allocates memeory to video once it has been uploaded
-			pixelout = new unsigned char[width*height * 3];//Assigns length to array
-			pixelin = new unsigned char[width*height * 3];
-			allocate = true;
-		}
-
-		if (cycleOn) {
-			cycle();//cycle throguh all the frames starting at 0 for recording
-		}
-		
-		if (newFrame) { //Check if new there is new frame
-			addNewFrame(); //Add new frame once passed through the filters
-		}
-	}
-}
-
-//--------------------------------------------------------------
-void ofApp::draw() {
-
-	ofBackground(125);
-	ofSetColor(255);
-
-	if(toggle)//Activated for 360 preview
-	cam.begin();
-
-	for (i = 0; i < videos.size(); i++)
-		videos[i].draw((ofGetWidth() / videos.size()*i), 0, ofGetWidth() / videos.size(), ofGetHeight() / videos.size());
-
-
-	if (videos.size() > 0) { //If there are videos preform recording functions
-
-
-		if (preLoad) { //Stops recording if ran out of frames
-
-			recorder.stopThread();
-			rec = false;
-
-			//Get directory size
-			dir.listDir("recording1");
-
-			sequence.loadSequence("recording1/frame", "bmp", 0, dir.size() - 1, 4);
-			sequence.preloadAllFrames();	//this way there is no stutter when loading frames
-			sequence.setFrameRate(5); //set to ten frames per second
-			sequenced = true; //Plays sequence when it has been written
-
-			for (int j = 0; j < videos.size(); j++)
-				skip[j] = true; //Prevents original videos from playing because composite has loaded, saving memory
-
-			preLoad = false;
-		}
-	}
-
-	if (sequenced) //Checks if sequence has been created, plays it
-		sequence.getFrameAtPercent(phasor(0.005, 0, 1))->draw(ofGetWidth()*0.5, ofGetHeight()*0.5, ofGetWidth()*0.5, ofGetHeight()*0.5);
-
-
-	if (toggle) { //Activated for 360 preview
-		wrapSphere();
-		cam.end();
-	}
-
-
-}
-
-void ofApp::exit() {
-	recorder.waitForThread();
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-
-	if (key == 'r') { //Recording
-		cycleOn = true;//cycleOns through frames individually when recording
-
-		recorder.setCounter(0);
-		for (i = 0; i < videos.size(); i++)
-			videos[i].setFrame(0);
-		rec = true;
-		recorder.startThread(false, true);
-
-
-		for (int j = 0; j < videos.size(); j++)
-			skip[j] = false;
-
-		for (i = 0; i < videos.size(); i++)
-			videos[i].setPaused(true);
-	}
-
-	if (key == 't') { //Turns on sphere and camera controls
-		cam.toggleControl();
-		toggle = !toggle;
-	}
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo info) {
-
-	//Gets video path
-	path = info.files[0];
-
-	//Creates new video player
-	ofVideoPlayer player;
-	player.loadMovie(path);
-	player.setLoopState(OF_LOOP_NORMAL);
-	player.play();
-	videos.push_back(player);
-
-}
-
-//--------------------------------------------------------------
-
-double ofApp::phasor(double frequency, double startphase, double endphase) {
-	//This is a phasor that takes a value for the start and end of the ramp. 
-	if (phase<startphase) {
-		phase = startphase;
-	}
-	if (phase >= endphase) phase = startphase;
-	phase += ((endphase - startphase) / (1 / (frequency)));
-	return(phase);
-}
-
-
-//--------------------------------------------------------------
-void ofApp::wrapSphere() {
-
-	ofSetColor(255, 255, 255);
-
-	sequence.getTextureReference().bind();
-
-	ofPushMatrix();
-
-	ofRotateX(90);
-
-	glMatrixMode(GL_TEXTURE);
-	glPushMatrix();
-	ofScale(sequence.getWidth(), sequence.getHeight());
-	glMatrixMode(GL_MODELVIEW);
-
-	glEnable(GL_DEPTH_TEST); //enable depth comparisons NEEDED FOR SPHERE DEPTH
-	ofDisableArbTex(); //needed for textures to work with gluSphe
-
-	gluSphere(quadric, 200, 100, 100);
-
-	glDisable(GL_DEPTH_TEST);//disable depth comparisons NEEDED FOR FLAT MASK
-	ofEnableArbTex(); //needed for textures to work with gluSphe
-
-
-	glMatrixMode(GL_TEXTURE);
-	glPopMatrix();
-	glMatrixMode(GL_MODELVIEW);
-
-	sequence.getTextureReference().unbind();
-
-	ofPopMatrix();
-}
-
-
-//--------------------------------------------------------------
-void ofApp::addNewFrame() {
-
-	for (u = 0; u < videos.size(); u++) {
-		if (rec) {
-			pixelin = videos[u].getPixels();//Gets pixels from video
-
-			for (i = 0; i < width; i++) {
-				for (j = 0; j < height; j++) { //Writes pixels if chroma trheshold hasn't been reached, otherwise assumes from previous frame
-
-
- 					thre = 160;
-
-					if (u == 0 || pixelin[(j*width + i) * 3 + 1] < thre || pixelin[(j*width + i) * 3 + 2] > 150) {
-						pixelout[(j*width + i) * 3 + 0] = pixelin[(j*width + i) * 3 + 0];//red
-						pixelout[(j*width + i) * 3 + 1] = pixelin[(j*width + i) * 3 + 1];//green
-						pixelout[(j*width + i) * 3 + 2] = pixelin[(j*width + i) * 3 + 2];//blue
-					}
 				}
 			}
-			preview.setFromPixels(pixelout, width, height, OF_IMAGE_COLOR);//Writes pixels to preview sequence
 		}
-
 	}
-	//Outside of loop to make sure final edit is layered correctly
-	recorder.addFrame(preview);
-	newFrame = false;
+
+	preview.setFromPixels(pixelout, width, height, OF_IMAGE_COLOR);//Writes pixels to preview sequence
+
 }
 
 //--------------------------------------------------------------
-void ofApp::cycle() { //cycle throguh all the frames starting at 0 for recording
-	for (i = 0; i < videos.size(); i++) {
+void ofApp::draw(){
 
-		videos[i].nextFrame();
-		newFrame = true;
+	preview.draw(0, 0, img[0].getWidth()*0.5, img[0].getHeight()*0.5);
 
-		if (videos[0].getCurrentFrame() >= videos[0].getTotalNumFrames() || videos[0].getPosition() >= 0.99) {
-			videos[i].setFrame(0);
-			cycleOn = false;
-			preLoad = true;
-		}
-	}
+	gui.draw();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+
+int ofApp::convolution(int x, int y, double matrix[25], int matrixsize, int iteration, unsigned char * pixels) {
+
+	float sum = 0; // Kernel sum for this pixel
+
+
+			for (int ky = -range; ky <= range; ky++) {
+				for (int kx = -range; kx <= range; kx++) {
+
+					// Calculate the adjacent pixel for this kernel point
+					//int pos = (y + ky) * width + (x + kx) * 3 + iteration;
+					//Reads 1D array as 2D
+					int pos = ((y + ky)*width + (x + kx))*3 + iteration;
+
+					// Image is grayscale, red/green/blue are identical
+					float val = pixels[pos];
+
+					//Reads 1D array as 2D
+					int count = j * matrixsize + i;
+
+					// Multiply adjacent pixels based on the kernel values
+					sum += matrix[count] * val;
+				}
+			}
+	
+	sum = ofConstrain(sum, 0, 255);
+	return sum;
+}
+
+//--------------------------------------------------------------
+float ofApp::ofConstrain(float amt, float low, float high) {
+	return (amt < low) ? low : ((amt > high) ? high : amt);
 }
--- ofApp.h
+++ ofApp.h
@@ -1,69 +1,68 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofxImageSequence.h"
-#include "ofxImageSequenceRecorder.h"
-#include "ofxFirstPersonCamera.h"
+#include "ofxGui.h"
 
-class ofApp : public ofBaseApp {
+class ofApp : public ofBaseApp{
 
-public:
-	~ofApp();/* destructor is very useful */
-	void setup();
-	void update();
-	void draw();
+	public:
+		void setup();
+		void update();
+		void draw();
 
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo info);
-	void gotMessage(ofMessage msg);
-	double phasor(double frequency, double startphase, double endphase);
-	void wrapSphere();
-	void addNewFrame();
-	void cycle();
-	void exit();
-
-	string path; //Path of dragged file
-
-				 //Contain videos
-	vector <ofVideoPlayer> videos;
-	ofImage preview;
-
-	unsigned char * pixelout;
-	unsigned char * pixelin;
-	unsigned char * pixels;
-
-	int i, j, u; //Iterators
-	int s; //size
-	int width, height;
-	int thre; //Threshold for chroma
-
-	bool skip[9]; //Skips updates
-	bool write = false;//writes frames to preview
-
-	ofxImageSequence sequence;
-	double phase;
-
-	//Recorder
-	ofxImageSequenceRecorder recorder;
-	bool rec = false;
-	bool allocate = false;
-	bool sequenced = false;
-	bool newFrame = false;
-	bool cycleOn = false;
-	bool preLoad = false;
-	bool toggle = false;
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		int convolution(int x, int y, double matrix[9], int matrixsize, int iteration, unsigned char * pixels);
+		float ofConstrain(float amt, float low, float high);
 
 
-	ofDirectory dir;
 
+		unsigned char * pixelout;
+		unsigned char * pixelin;
+		unsigned char * pixels[2];
 
-	//360 Vision
-	GLUquadricObj *quadric;
-	ofxFirstPersonCamera cam;
+		ofImage img[2];
+		ofImage preview;
+
+		int i, j, u;
+		int width, height;
+
+		int pointer[3];
+
+		int w = 80;
+
+		float v = 1.0 / 25.0;
+
+		// The convolution matrix for a "sharpen" effect stored as a 3 x 3 two-dimensional array.
+		
+		
+		double matrix[25] = { v, v, v, v, v, 
+			                 v, v, v, v, v,
+		                   	 v, v, v, v, v,
+		                     v, v, v, v, v,
+		                     v, v, v, v, v};
+						
+				/*
+		double matrix[9] = {0, -1, 0,
+		                    -1, 5, -1,
+		                    0, -1, 0};
+			
+		*/	
+
+		ofxIntSlider thre;
+		ofxPanel gui;
+
+		int red, green, blue;
+
+		int matrixsize, range;
+
 };
